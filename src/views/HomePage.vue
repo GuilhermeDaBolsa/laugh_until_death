c838efdcbc2d8000d6b09c2b8f21ab0ca0b196e0
GuilhermeDaBolsa <Guilherme_Giordani@live.com>
2022-03-13 18:44:13 -0300

    removing vue standard components

---
+++ src/views/HomePage.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>oi</div>
+</template>
+
+<script>
+export default {
+    props: {},
+    mixins: {},
+    data(){
+        return {
+        }
+    },
+    directives: {},
+    components: {},
+    computed: {},
+    watch: {},
+    methods: {},
+}
+</script>
+
+<style scoped>
+</style>
