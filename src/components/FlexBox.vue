fbb0151636e44f81f1f6316a212d3347d162a6df
GuilhermeDaBolsa <Guilherme_Giordani@live.com>
2022-03-18 19:28:17 -0300

    creating generic flexBox component

---
+++ src/components/FlexBox.vue
@@ -0,0 +1,24 @@
+<template>
+	<div style="display: flex; flex-direction: column; flex: 1; justify-content: center; align-items: center;">
+		<slot></slot>
+	</div>
+</template>
+
+<script>
+export default {
+    props: {},
+    mixins: {},
+    data(){
+        return {
+        }
+    },
+    directives: {},
+    components: {},
+    computed: {},
+    watch: {},
+    methods: {},
+}
+</script>
+
+<style scoped>
+</style>
