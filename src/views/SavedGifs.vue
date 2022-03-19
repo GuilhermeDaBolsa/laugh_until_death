d0e77030a28aa735b9f46f72a58fc468653421e3
GuilhermeDaBolsa <Guilherme_Giordani@live.com>
2022-03-19 10:52:53 -0300

    Showing saved gifs in SavedGif page

--- src/views/SavedGifs.vue
+++ src/views/SavedGifs.vue
@@ -4,31 +4,57 @@
       <router-link to="/">Encontrar GIF</router-link> |
       <router-link to="/saved">Meus GIFs</router-link>
     </nav>
-oi
+
+	<FlexBox v-if="loading">
+		<div class="ui active inline loader"></div>
+		<div>Carregando...</div>
+	</FlexBox>
+
+	<FlexBox v-else-if="errorMessage" style="flex-direction: row;">
+		<i class="large red exclamation triangle icon"></i> {{errorMessage}}
+	</FlexBox>
+
+	<FlexBox v-else-if="savedGifs.length == 0">
+		<div>Você ainda não salvou nenhum GIF (emoji de alguma coisa)</div>
+	</FlexBox>
+
+	<GifCardList
+		v-else
+		:gifsList="savedGifs"
+		@gifSelect="1==1"
+		@reachedBottomOfList="1==1"
+	/>
+
 	</div>
 </template>
 
 <script>
+import FlexBox from '@/components/FlexBox.vue';
+import GifCardList from '@/components/GifCardList.vue';
+
 export default {
     props: {},
     mixins: {},
     data(){
         return {
-			gifNameInput: "",
+			loading: false,
+			errorMessage: "",
         }
     },
     directives: {},
-    components: {},
-    computed: {},
-    watch: {},
-    methods: {
-		searchGifs(gifName) {
-			if(!gifName)
-				return;
-
-			console.log(gifName);
+    components: {
+		FlexBox,
+		GifCardList
+	},
+    computed: {
+		savedGifs: {
+			get(){
+				return this.$store.getters["SavedGifs/savedGifs"];
+			}
 		}
 	},
+    watch: {},
+    methods: {},
 }
 </script>
 
